78f818e6941c89ae71c08531b582deec8dac938d
fenistil <fenistil.eve@gmail.com>
2011-02-20 14:24:55 +0100

    common.h and c have been added for port communication and some typedefs

---
+++ kernel/common.h
@@ -0,0 +1,16 @@
+#ifndef _COMMON_H
+#define _COMMON_H
+
+/* some typedefs */
+typedef unsigned 	int 	u32int;
+typedef				int		s32int;
+typedef unsigned	short	u16int;
+typedef 			short	s16int;
+typedef unsigned	char	u8int;
+typedef				char	s8int;
+
+void outb(u16int port, u8int value);
+u8int inb(u16int port);
+u16int inw(u16int port);
+
+#endif
