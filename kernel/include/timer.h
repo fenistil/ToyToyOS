993a97338573d836ef19f91d65a1dc9aa741d541
fenistil <fenistil.eve@gmail.com>
2011-02-27 23:07:09 +0100

    oops forgot to add these files :)

---
+++ kernel/include/timer.h
@@ -0,0 +1,8 @@
+#ifndef _H_TIMER
+#define _H_TIMER
+
+#include "common.h"
+
+void init_timer(u32int_t freq);
+
+#endif
---
+++ kernel/timer.c
@@ -0,0 +1,23 @@
+#include "timer.h"
+#include "descriptors.h"
+#include "scrn.h"
+
+u32int_t tick = 0;
+
+static void timer_callback(registers_t regs) {
+	tick++;
+	regs = regs;
+}
+
+void init_timer(u32int_t freq) {
+	register_interrupt_handler(IRQ_TIMER, &timer_callback);
+
+	u32int_t divisor = 1193182 / freq;
+
+	outb(0x43, 0x36);
+	u8int_t l = (u8int_t)(divisor & 0xFF);
+	u8int_t h = (u8int_t)((divisor >> 8) & 0xFF);
+
+	outb(0x40, l);
+	outb(0x40, h);
+}
